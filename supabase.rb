e99882638a1edc76722edd797490b0efe640b04e
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-03 03:14:24 +0800

    Brew formula update for supabase version v0.7.0

--- supabase.rb
+++ supabase.rb
@@ -5,29 +5,29 @@
 class Supabase < Formula
   desc "Supabase CLI"
   homepage "https://supabase.io"
-  version "0.6.2"
+  version "0.7.0"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/supabase/cli/releases/download/v0.6.2/supabase_0.6.2_darwin_amd64.tar.gz"
-      sha256 "7036da8eb5bd81e8c92ed74c20fece838739e60b9803b1c258cc6a0a980573d7"
+      url "https://github.com/supabase/cli/releases/download/v0.7.0/supabase_0.7.0_darwin_amd64.tar.gz"
+      sha256 "3c6c22b4e24745e492c21b0a3d4df2a25b7c4718cc658d90be7edd0b5844c945"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/supabase/cli/releases/download/v0.6.2/supabase_0.6.2_darwin_arm64.tar.gz"
-      sha256 "bcf0c406c1fe4b83e52c40099c4914cb8783d09cab2f267974881d529e9f8301"
+      url "https://github.com/supabase/cli/releases/download/v0.7.0/supabase_0.7.0_darwin_arm64.tar.gz"
+      sha256 "1a6ea67a1966dc24783ebb27394f4db5350fba59440f4089a2474e2b87d1eb2b"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/supabase/cli/releases/download/v0.6.2/supabase_0.6.2_linux_amd64.tar.gz"
-      sha256 "7a0975b5ce463a4e13753a54b16ae3affdcc116a8d2711bfc1d529fa15e023e8"
+      url "https://github.com/supabase/cli/releases/download/v0.7.0/supabase_0.7.0_linux_amd64.tar.gz"
+      sha256 "780733ee3d25ff053e4871cab8d4a44aec3f0c94645d47bc00bd520333fbec4b"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/supabase/cli/releases/download/v0.6.2/supabase_0.6.2_linux_arm64.tar.gz"
-      sha256 "eaca98274ecad21749710c9c2ea428d99829d157a67c09672f64fa7193a53b53"
+      url "https://github.com/supabase/cli/releases/download/v0.7.0/supabase_0.7.0_linux_arm64.tar.gz"
+      sha256 "7b9479d98aa0b670a5df7ad8fe69a7c6b7dbb450e8e089419858357d884fce23"
     end
   end
 

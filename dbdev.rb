6b367fc525475c09f336d9b0b1d409c2f10d1529
dbdev release-homebrew-tap.yaml workflow <raminder@supabase.io>
2023-10-25 06:39:43 +0000

    dbdev brew formula update for version v0.1.3

--- dbdev.rb
+++ dbdev.rb
@@ -2,46 +2,46 @@
 # frozen_string_literal: true
 
 class Dbdev < Formula
-    desc "Dbdev CLI"
-    homepage "https://database.dev/"
-    version "0.1.2"
-    license "MIT"
-  
-    on_macos do
-      if Hardware::CPU.arm?
-        url "https://github.com/supabase/dbdev/releases/download/v0.1.2/dbdev-v0.1.2-macos-amd64.tar.gz"
-        sha256 "575278d7e1718ee8054753a3d2952d70051fb1ccf8ec833f3886254f2dbb93b6"
-  
-        def install
-          bin.install "dbdev"
-        end
-      end
-      if Hardware::CPU.intel?
-        url "https://github.com/supabase/dbdev/releases/download/v0.1.2/dbdev-v0.1.2-macos-amd64.tar.gz"
-        sha256 "575278d7e1718ee8054753a3d2952d70051fb1ccf8ec833f3886254f2dbb93b6"
-  
-        def install
-          bin.install "dbdev"
-        end
+  desc "Dbdev CLI"
+  homepage "https://database.dev/"
+  version "0.1.3"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/supabase/dbdev/releases/download/v0.1.3/dbdev-v0.1.3-macos-amd64.tar.gz"
+      sha256 "da4412fc311e82d880b2fec1b3da33ec0b9c15ec1cbbf456d1736f53fe3069a1"
+
+      def install
+        bin.install "dbdev"
       end
     end
-  
-    on_linux do
-      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-        url "https://github.com/supabase/dbdev/releases/download/v0.1.2/dbdev-v0.1.2-linux-arm64.tar.gz"
-        sha256 "f270ed19c5bc9712226cf1dd1335c733a49b65b6b7c75a99bc62a4e6ae0d568a"
-  
-        def install
-          bin.install "dbdev"
-        end
-      end
-      if Hardware::CPU.intel?
-        url "https://github.com/supabase/dbdev/releases/download/v0.1.2/dbdev-v0.1.2-linux-amd64.tar.gz"
-        sha256 "93f7ab6c2f97294ff199f78dbd4567a5927770ad798cc37b668de2e3728d2c0b"
-  
-        def install
-          bin.install "dbdev"
-        end
+    if Hardware::CPU.intel?
+      url "https://github.com/supabase/dbdev/releases/download/v0.1.3/dbdev-v0.1.3-macos-amd64.tar.gz"
+      sha256 "da4412fc311e82d880b2fec1b3da33ec0b9c15ec1cbbf456d1736f53fe3069a1"
+
+      def install
+        bin.install "dbdev"
       end
     end
   end
+
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/supabase/dbdev/releases/download/v0.1.3/dbdev-v0.1.3-linux-arm64.tar.gz"
+      sha256 "23b572001fdf94f10f01b31e0e88f778d3f53cbaa9bc3fca26b342b0a273f1f1"
+
+      def install
+        bin.install "dbdev"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/supabase/dbdev/releases/download/v0.1.3/dbdev-v0.1.3-linux-amd64.tar.gz"
+      sha256 "d397589028475d2b430d77e8785527fbd0eb6d07e379a0c5cf1bc35246fc2fe4"
+
+      def install
+        bin.install "dbdev"
+      end
+    end
+  end
+end
